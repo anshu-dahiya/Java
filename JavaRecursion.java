014cc04de72e08eb0716e315fa133eae18310bbe
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2024-12-29 21:52:34 +0530

    Java Recursion

--- JavaRecursion.java
+++ JavaRecursion.java
@@ -1,2 +1,39 @@
+////Problem 1 (Factorial Of Value)
+//public class JavaRecursion {
+//    static int factorial(int n){
+//        if(n==0 || n==1){
+//            return 1;
+//        }
+//        else{
+//            return n * factorial(n-1);
+//        }
+//    }
+//    public static void main(String[] args){
+//        int x= 5;
+//        System.out.println("The factorial of "+ x +"
+//        is: " + factorial(x));
+//    }
+//}
+
+
+//Problem 2 (Fibonacci Sequence)
 public class JavaRecursion {
+    static int fibonacci(int n){
+        if(n==0){
+            return 0;
+        }
+        else if (n==1) {
+            return 1;
+        }
+        else{
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+    public static void main(String[] args){
+        int terms = 10;
+        System.out.println("Fibonaaci Sequence up to "+ terms +" terms is:");
+        for(int i=0; i<terms; i++){
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
 }
