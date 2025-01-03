6f86cf48f010b69239885af18d70f0cee620a3bf
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-01-03 22:16:11 +0530

    Java Recursion and Methods Practice

--- JavaRecursion.java
+++ JavaRecursion.java
@@ -40,16 +40,59 @@
 
 
 //Problem 3(Calculate sum of first n Natural number)
-public class JavaRecursion {
-    static int sumNatural(int n){
-        if(n==1){
-            return n;
-        }
-        return n + sumNatural(n-1);
-    }
-    public static void main(String[] args){
+//public class JavaRecursion {
+//    static int sumNatural(int n){
+//        if(n==1){
+//            return 1;
+//        }
+//
+//        return n + sumNatural(n-1);
+//    }
+//    public static void main(String[] args){
+//
+//        int sum = sumNatural(6);
+//        System.out.println(sum);
+//    }
+//}
 
-        int sum = sumNatural(6);
-        System.out.println(sum);
+
+//Problem 4(Recursive Function to Print following Pattern)
+//*
+//**
+//***
+//****
+//public class JavaRecursion {
+//    static void pattern1(int n){
+//        if(n>0){
+//            pattern1(n-1);
+//            for(int i=0; i<n; i++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    public static void main(String[]args){
+//        pattern1(5);
+//    }
+//}
+
+
+//Problem 5(Recursive Function to Print following Pattern)
+//****
+//***
+//**
+//*
+public class JavaRecursion {
+    static void pattern2(int n){
+        if(n>0){
+            for(int i=0; i<n; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+            pattern2(n-1);
+        }
+    }
+    public static void main(String[]args){
+        pattern2(5);
     }
 }
--- MethodsPractice.java
+++ MethodsPractice.java
@@ -11,28 +11,42 @@
 //}
 
 
-//Problem 2(Method to Print following Pattern)
+//Problem 2(Function to Print following Pattern)
 //*
 //**
 //***
 //****
-public class MethodsPractice {
-    static void pattern1(int n){
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i+1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-    public static void main(String[] args){
-        pattern1(7);
-    }
-}
-
-
-//Problem 3
 //public class MethodsPractice {
-//
+//    static void pattern1(int n){
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<i+1; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    public static void main(String[] args){
+//        pattern1(7);
+//    }
 //}
+
+
+//Problem 3(Function to Print following Pattern)
+//****
+//***
+//**
+//*
+public class MethodsPractice {
+   static void pattern2(int n){
+       for(int i=n-1; i>=0; i--){
+           for(int j=0; j<i+1; j++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+   }
+   public static void main(String[] args){
+       pattern2(5);
+   }
+}
 
